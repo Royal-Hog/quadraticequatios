837969da6e549b20a1debdf4c3d1c9278394ecd9
ASUS <ASUS@DESKTOP-2FDU944>
2023-03-02 01:46:28 +0300

    Добавьте файлы проекта.

---
+++ quadraticequatios.cpp
@@ -0,0 +1,35 @@
+﻿#include<fstream>
+#include<iostream>
+using namespace std;
+double a, b, c,d,r1, r2;
+void openfile(string adress) {
+    ifstream fin(adress);
+    fin >> a >> b >> c;
+    fin.close();
+}
+void closefile(string adress) {
+    ofstream fout(adress);
+    if (d < 0) {
+        fout << "вещественных корней нет";
+    }
+    else {
+
+        fout << r1 << ' ' << r2;
+    }
+    fout.close();
+
+}
+double discriminant() {
+    return  b * b - 4 * a * c;
+}void roots() {
+    r1 = (-b + sqrt(d)) / 2;
+    r2 = (-b - sqrt(d)) / 2;
+}
+int main()
+{
+    openfile("input.txt");
+    d = discriminant();
+    roots();
+    closefile("output.txt");
+ 
+}
